37c4fb66034234a78dad15d9401a2830a2e3792b
PetraOmrcen <petra.omrcen1@gmail.com>
2021-07-18 15:19:56 +0200

    Launch scree, login screen unfinished

--- TV Shows/TV Shows/Login/LoginViewController.swift
+++ TV Shows/TV Shows/Login/LoginViewController.swift
@@ -9,29 +9,17 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
-    private var counter = 0
-
-    @IBOutlet private weak var titleLabel: UILabel!
-    @IBOutlet private weak var button: UIButton!
-    @IBOutlet private weak var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet private weak var LoginButton: UIButton!
+    @IBOutlet private weak var RegisterButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
     
-        // adding round corners to button
-        button.layer.cornerRadius = 25.0
+        
+        LoginButton.layer.cornerRadius = 15.0
+        
     }
     
-    @IBAction private func buttonActionHandler(_ sender: Any) {
-        print("Button pressed")
-        counter += 1
-        titleLabel.text = String(counter)
-        if activityIndicator.isAnimating {
-            
-            activityIndicator.stopAnimating()
-        } else {
-            activityIndicator.startAnimating()
-        
-        }
-    }
+   
+    
 }
