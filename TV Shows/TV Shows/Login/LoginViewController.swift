48e224e51dc45725c0b00f71ccd9ecde8a10fa4a
PetraOmrcen <petra.omrcen1@gmail.com>
2021-07-20 06:55:21 +0200

    Email and password lines

--- TV Shows/TV Shows/Login/LoginViewController.swift
+++ TV Shows/TV Shows/Login/LoginViewController.swift
@@ -12,19 +12,32 @@ class LoginViewController: UIViewController {
     @IBOutlet private weak var LoginButton: UIButton!
     @IBOutlet private weak var RegisterButton: UIButton!
     @IBOutlet private weak var CheckBoxButton: UIButton!
+    @IBOutlet private weak var EmailTextField: UITextField!
+    @IBOutlet private weak var PasswordTextField: UITextField!
+    
+    
+    private var state = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    
         
         LoginButton.layer.cornerRadius = 15.0
-        
+        EmailTextField.attributedPlaceholder = NSAttributedString(string:"username",
+                                                                     attributes:[NSAttributedString.Key.foregroundColor: UIColor.white])
+        PasswordTextField.attributedPlaceholder = NSAttributedString(string:"password",
+                                                                     attributes:[NSAttributedString.Key.foregroundColor: UIColor.white])
     }
     
-    @IBAction private func CheckBoxButtonAction(_ sender: Any) {
-        
-        
+    
+    @available(iOS 13.0, *)
+    @IBAction func CheckBoxButtonAction(_ sender: Any) {
+        if state == 0 {
+            //CheckBoxButton.setBackgroundImage(, for: .normal)
+            state = 1
+        } else{
+            //CheckBoxButton.setBackgroundImage(, for: .normal)
+            state = 0
+        }
+    
     }
-    
-    
 }
