28374027b4e69f0f009e8cf30b69891666851d8d
PetraOmrcen <petra.omrcen1@gmail.com>
2021-07-21 18:18:08 +0200

    Added eye and checkbox

--- TV Shows/TV Shows/Login/LoginViewController.swift
+++ TV Shows/TV Shows/Login/LoginViewController.swift
@@ -9,35 +9,50 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
-    @IBOutlet private weak var LoginButton: UIButton!
-    @IBOutlet private weak var RegisterButton: UIButton!
-    @IBOutlet private weak var CheckBoxButton: UIButton!
-    @IBOutlet private weak var EmailTextField: UITextField!
-    @IBOutlet private weak var PasswordTextField: UITextField!
     
+    // MARK: - Outlets
+
+    @IBOutlet private weak var loginButton: UIButton!
+    @IBOutlet private weak var registerButton: UIButton!
+    @IBOutlet private weak var checkBoxButton: UIButton!
+    @IBOutlet private weak var emailTextField: UITextField!
+    @IBOutlet private weak var passwordTextField: UITextField!
+    @IBOutlet private weak var eyeButton: UIButton!
     
-    private var state = 0
+    // MARK: - Lifecycle methods
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        LoginButton.layer.cornerRadius = 15.0
-        EmailTextField.attributedPlaceholder = NSAttributedString(string:"username",
+        emailTextField.textColor = UIColor.white
+        passwordTextField.textColor = UIColor.white
+        checkBoxButton.setImage(UIImage(named: "ic-checkbox-unselected"), for: .normal)
+        eyeButton.setImage(UIImage(named: "open_eye"), for: .normal)
+        loginButton.layer.cornerRadius = 15.0
+        emailTextField.attributedPlaceholder = NSAttributedString(string:"username",
                                                                      attributes:[NSAttributedString.Key.foregroundColor: UIColor.white])
-        PasswordTextField.attributedPlaceholder = NSAttributedString(string:"password",
+        passwordTextField.attributedPlaceholder = NSAttributedString(string:"password",
                                                                      attributes:[NSAttributedString.Key.foregroundColor: UIColor.white])
     }
     
+    // MARK: - Actions
     
     @available(iOS 13.0, *)
     @IBAction func CheckBoxButtonAction(_ sender: Any) {
-        if state == 0 {
-            //CheckBoxButton.setBackgroundImage(, for: .normal)
-            state = 1
-        } else{
-            //CheckBoxButton.setBackgroundImage(, for: .normal)
-            state = 0
+        if checkBoxButton.image(for: .normal) ==  UIImage(named: "ic-checkbox-selected"){
+            checkBoxButton.setImage(UIImage(named: "ic-checkbox-unselected"), for: .normal)
+        }else{
+            checkBoxButton.setImage(UIImage(named: "ic-checkbox-selected"), for: .normal)
         }
+    }
     
+    @IBAction func hideShowPasswordAction(_ sender: Any) {
+        if eyeButton.image(for: .normal) == UIImage(named: "open_eye"){
+            passwordTextField.isSecureTextEntry = !passwordTextField.isSecureTextEntry
+            eyeButton.setImage(UIImage(named: "closed_eye"), for: .normal)
+        }else{
+            passwordTextField.isSecureTextEntry = !passwordTextField.isSecureTextEntry
+            eyeButton.setImage(UIImage(named: "open_eye"), for: .normal)
+        }
     }
 }
